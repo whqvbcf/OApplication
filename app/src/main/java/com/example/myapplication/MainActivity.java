3360f5e62545f9f27d65b8817cdb6391824e631b
hqwang9 <hqwang9@iflytek.com>
2025-04-20 19:59:52 +0800

    spinner的弹框背景透明

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -60,6 +60,11 @@ public class MainActivity extends Activity {
 //        spinner.setDropDownHorizontalOffset(-offset);
         Log.d(LOG_TAG, "=======screenWidth = " + screenWidth + " offset = " + offset);
 
+        spinner.setBackgroundResource(android.R.color.transparent);
+        spinner.setBackgroundColor(Color.TRANSPARENT);
+
+        spinner.getPopupBackground().setAlpha(0);//okki
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
